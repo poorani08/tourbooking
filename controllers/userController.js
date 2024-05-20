b20fc454d4894fb4c978ecc72ef61c85fb76c494
poorani08 <123159672+poorani08@users.noreply.github.com>
2024-05-20 19:42:51 +0530

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+import User from '../models/User.js'
+export const createUser =async(req,res)=>{
+    const newUser=new User(req.body)
+    try{
+        const savedUser=await newUser.save()
+        res.status(200).json({success:true,message:'successfully created',data:savedUser})
+    }
+    catch(err){
+        res.status(500).json({success:false,message:"failed to create"})
+
+    }
+}
+
+export const updateUser=async(req,res)=>{
+    const id=req.params.id
+    try{
+        const updatedUser=await User.findByIdAndUpdate(id,{
+            $set:req.body
+        },{new:true})
+
+        res.status(200).json({success:true,message:'successfully updated',data:updatedUser})
+}catch(err){
+    res.status(500).json({success:false,message:"failed to update"})
+
+    }
+}
+
+export const deleteUser=async(req,res)=>{
+    const id=req.params.id
+    try{
+        await User.findByIdAndDelete(id)
+
+        res.status(200).json({success:true,message:'successfully deleted',})
+}catch(err){
+    res.status(500).json({success:false,message:"failed to delete",})
+
+    }
+}
+export const getSingleUser=async(req,res)=>{
+    const id=req.params.id
+    try{
+        const user=await User.findById(id)
+
+        res.status(200).json({success:true,message:'successfully get',data:user})
+}catch(err){
+    res.status(404).json({success:false,message:"failed to get"})
+
+    }
+    
+}
+
+export const getAllUser=async(req,res)=>{
+   
+  
+    try{
+        const users=await User.find({})
+        res.status(200).json({success:true,message:'successfully ',data:users,})
+}catch(err){
+    res.status(404).json({success:false,message:"failed to get"})
+
+    }
+
+    
+};
